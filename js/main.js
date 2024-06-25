7f66c222b90eb91c240c2d778ad03f48f74b0c4b
Francescosommellaa <franc.sommella@gmail.com>
2024-06-25 15:31:08 +0200

    completa e consegna

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            messaggio: 'Hello Vue',
+            immagineUrl: 'https://th.bing.com/th/id/OSK.HEROBxbO7xPkY5sqXDuxUDrqwCAkMP71Y1sC5om9w9M2NV4?rs=1&pid=ImgDetMain'
+        }
+    }
+})
+app.mount('#app')
